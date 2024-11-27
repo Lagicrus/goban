8e454648a3c21a597712edfdafef7e360d1393f9
Lagicrus <Lagicrus@users.noreply.github.com>
2024-11-27 15:01:58 +0000

    Add last test

--- src/index.ts
+++ src/index.ts
@@ -156,7 +156,22 @@ function test_black_shape_is_not_taken_when_it_has_a_liberty() {
   console.log(`Test 8, should be false: ${goban.is_taken(1, 2)}`);
 }
 
+function test_square_shape_is_taken() {
+  const goban = new Goban([
+    'oo.',
+    '##o',
+    '##o',
+    'oo.',
+  ])
+
+  console.log(`Test 9, should be true: ${goban.is_taken(0, 1)}`);
+  console.log(`Test 10, should be true: ${goban.is_taken(0, 2)}`);
+  console.log(`Test 11, should be true: ${goban.is_taken(1, 1)}`);
+  console.log(`Test 12, should be true: ${goban.is_taken(1, 2)}`);
+}
+
 test_white_is_taken_when_surrounded_by_black();
 test_white_is_not_taken_when_it_has_a_liberty();
 test_black_shape_is_taken_when_surrounded();
 test_black_shape_is_not_taken_when_it_has_a_liberty();
+test_square_shape_is_taken();
